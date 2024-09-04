286e7beb74685c930272fbeac4e93db4888b968d
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-09-04 11:17:36 +0200

    edit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   // Non è necessario specificare 'base' per la root del dominio
 })
