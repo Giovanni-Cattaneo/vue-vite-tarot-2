953e9a46def130ba7bd151c9daa5554c58ff6dea
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-09-04 11:15:31 +0200

    vite config edit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/vue-vite-tarot-2/'  // Nome del repository GitHub Pages
-    : '/'
+  // Non è necessario specificare 'base' per la root del dominio
 })
